30fe7f2f86444ff333a0e0ca8074e133c5290fec
Pranava Ninadam <pranavaninadam@Pranavas-MacBook-Air.local>
2026-08-24 09:12:45 +0530

    initial commit

---
+++ config.js
@@ -0,0 +1,57 @@
+const INTRO_UNLOCK_TIME = "2026-08-28T00:00:00"
+const INTRO_MESSAGE = "Put The Intro Message here"
+
+const UNLOCK_TIME = "2026-08-29T12:00:00"
+
+const MEMBERS = [
+    {
+        name: "Navya",
+        pin: "2001",
+        lang: "en-IN",
+        body: "content goes here",
+        sign: "your fav child",
+        media: [
+            {url: "", alt: "funny gif", after: 0}
+        ]
+    },
+    {
+        name: "Lasya",
+        pin: "2005",
+        lang: "en-IN",
+        body: "content goes here",
+        sign: "your fav child",
+        media: [
+            {url: "", alt: "funny gif", after: 0}
+        ]
+    },
+    {
+        name: "Amma",
+        pin: "1975",
+        lang: "en-IN",
+        body: "content goes here",
+        sign: "your fav child",
+        media: [
+            {url: "", alt: "funny gif", after: 0}
+        ]
+    },
+    {
+        name: "Nannagaru",
+        pin: "1971",
+        lang: "en-IN",
+        body: "content goes here",
+        sign: "your fav child",
+        media: [
+            {url: "", alt: "funny gif", after: 0}
+        ]
+    },
+    {
+        name: "Nanamma",
+        pin: "1938",
+        lang: "en-IN",
+        body: "content goes here",
+        sign: "your fav child",
+        media: [
+            {url: "", alt: "funny gif", after: 0}
+        ]
+    },
+]
